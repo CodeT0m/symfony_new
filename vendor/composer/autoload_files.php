d384ffe87e4aebdc5b6472e90be53df12c5def9c
Sorin Ilin <sorinilin@MacBook-Pro.local>
2020-08-16 23:27:34 +0300

    add doctrine maker form bundles

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -13,4 +13,5 @@ return array(
     'e69f7f6ee287b969198c3c9d6777bd38' => $vendorDir . '/symfony/polyfill-intl-normalizer/bootstrap.php',
     'f598d06aa772fa33d905e87be6398fb1' => $vendorDir . '/symfony/polyfill-intl-idn/bootstrap.php',
     '667aeda72477189d0494fecd327c3641' => $vendorDir . '/symfony/var-dumper/Resources/functions/dump.php',
+    '6a47392539ca2329373e0d33e1dba053' => $vendorDir . '/symfony/polyfill-intl-icu/bootstrap.php',
 );
